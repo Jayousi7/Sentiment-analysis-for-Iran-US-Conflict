5cd148bad1c449325e0d07aa7e656ebca5ede6c4
Jayousi7 <a.jayousi91@gmail.com>
2026-05-18 00:24:25 +0300

    changed some configs for lstm

--- configs.py
+++ configs.py
@@ -1,11 +1,11 @@
 lstm_hyperparameters = {
-    'learning_rate': [1e-3, 5e-4],
+    'learning_rate': [1e-3, 1e-2, 1e-4],
     'batch_size': [16, 32],
-    'weight_decay': [1e-4, 1e-5],
+    'weight_decay': [0.01, 0.005],
     'dropout': [0.3, 0.5],
     'optimizer': ['Adam', 'AdamW'],
     'epochs': [50],
-    'patience': [5]
+    'patience': [10]
 }
 
 
